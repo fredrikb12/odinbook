a7a1bc584146482eb6c8a3af8cc2d19ea0dabbc0
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-09 13:45:39 +0200

    Create middleware for verifying jwt token, otherwise redirects to login

---
+++ middleware/jwtAuth.js
@@ -0,0 +1,23 @@
+const auth = require("../utils/auth");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const jwtAuth = (req, res, next) => {
+  const token = req.cookies.odinbooktoken;
+  if (!token)
+    return res.status(403).redirect("http://localhost:3000/login/facebook");
+  try {
+    const data = jwt.verify(token, process.env.JWT_SECRET);
+    console.log(data);
+    req.cookieToken = {
+      _id: data._id,
+      iat: data.iat,
+      exp: data.exp,
+    };
+    next();
+  } catch (e) {
+    return res.status(403).redirect("http://localhost:3000/login/facebook");
+  }
+};
+
+module.exports = jwtAuth;
