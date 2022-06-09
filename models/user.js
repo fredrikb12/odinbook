4c56c8bfd7b3bbe2ed92376700b673a2075f2d23
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-09 16:46:09 +0200

    Add fields to user model

--- models/user.js
+++ models/user.js
@@ -3,8 +3,13 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: { type: String, required: true, maxLength: 100, minLength: 1 },
-  facebook_id: { type: String },
+  facebook_id: { type: String, required: true },
   picture: { type: String, required: false },
+  friends: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+  requests: [
+    { type: Schema.Types.ObjectId, ref: "FriendRequest", required: true },
+  ],
+  posts: [{ type: Schema.Types.ObjectId, ref: "Post", required: true }],
 });
 
 module.exports = mongoose.model("User", UserSchema);
