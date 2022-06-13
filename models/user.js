333dca4ef0f3b7104c96dd503bde188872f29aeb
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-13 13:22:57 +0200

    Add default values

--- models/user.js
+++ models/user.js
@@ -5,11 +5,20 @@ const UserSchema = new Schema({
   name: { type: String, required: true, maxLength: 100, minLength: 1 },
   facebook_id: { type: String, required: true },
   picture: { type: String, required: false },
-  friends: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-  requests: [
-    { type: Schema.Types.ObjectId, ref: "FriendRequest", required: true },
+  friends: [
+    { type: Schema.Types.ObjectId, ref: "User", required: true, default: [] },
   ],
-  posts: [{ type: Schema.Types.ObjectId, ref: "Post", required: true }],
+  requests: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "FriendRequest",
+      required: true,
+      default: [],
+    },
+  ],
+  posts: [
+    { type: Schema.Types.ObjectId, ref: "Post", required: true, default: [] },
+  ],
 });
 
 module.exports = mongoose.model("User", UserSchema);
