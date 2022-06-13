1b798a7ba06dd552bac1ad73eb4eb4e0dbf34791
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-13 13:21:57 +0200

    Create new helper functions and refactor to only use async/await

--- db-helpers/mongodb.js
+++ db-helpers/mongodb.js
@@ -1,23 +1,18 @@
 const User = require("../models/user");
 
 exports.mongoDB = {
-  findOrCreateNewUser: (accessToken, refreshToken, profile, cb) => {
-    User.findOne({ facebook_id: profile.id }, function (err, user) {
-      if (err) return cb(err);
-      if (user) return cb(null, user);
-      else {
-        newUser = new User({
-          facebook_id: profile.id,
-          name: profile.displayName,
-        });
-        newUser.save(function (err) {
-          if (err) return cb(err);
-          else {
-            console.log("saving user...");
-            return cb(null, newUser);
-          }
-        });
-      }
-    });
+  findUser: async (query, cb) => {
+    const user = await User.findOne(query).catch((e) => cb(e));
+    if (user) return user;
+    else return null;
+  },
+  findUsers: async (query) => {
+    const users = await User.find(query || null);
+    return users;
+  },
+  createUser: async (user, cb) => {
+    const newUser = new User(user);
+    const savedUser = await newUser.save().catch((e) => cb(e));
+    return cb(null, savedUser);
   },
 };
