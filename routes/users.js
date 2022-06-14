a7480cb96fe0656f914d3d1b5b699f558d908af7
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-14 12:28:14 +0200

    Implement users and users_userId GET routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/user");
+const mongoDB = require("../db-helpers/mongodb").mongoDB;
+const {
+  users_GET,
+  users_userId_GET,
+} = require("../controllers/userController");
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get("/", users_GET);
+
+router.get("/:userId", users_userId_GET);
+
+/*router.post("/", async (req, res, next) => {
+  const user = mongodb.postUser({});
+});*/
 
 module.exports = router;
