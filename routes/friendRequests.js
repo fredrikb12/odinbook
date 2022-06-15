effda8d420a0cdb8f1ad9db7a12ef724da8a96cc
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-15 20:37:45 +0200

    Add friend requests post route

--- routes/friendRequests.js
+++ routes/friendRequests.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {
   friendRequests_POST,
+  friendRequests_DELETE,
 } = require("../controllers/friendRequestController");
 
 router.post("/", friendRequests_POST);
+
+//router.delete("/:friendRequestid", friendRequests_DELETE);
 
 module.exports = router;
