51cbeecefd15749bb9df2eb3debc1c19627e2aed
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-15 20:37:22 +0200

    Add users post route

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,13 @@ router.get("/", users_GET);
 
 router.get("/:userId", users_userId_GET);
 
+router.post("/", async (req, res, next) => {
+  const { name, facebook_id } = req.body;
+  const user = new User({ name, facebook_id });
+  const savedUser = await user.save().catch((e) => next(e));
+  return res.status(200).json({ statusCode: 200, user: savedUser });
+});
+
 /*router.post("/", async (req, res, next) => {
   const user = mongodb.postUser({});
 });*/
