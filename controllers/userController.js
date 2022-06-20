cec8bf9bbc1628a72121f90498eff521495ba8a0
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-20 17:03:00 +0200

    Refactor to use createResponse helper

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,18 @@
 const User = require("../models/user");
+const { createResponse } = require("../utils/responseHelpers");
 
 exports.users_GET = async (req, res, next) => {
   const users = await User.find().catch((e) => next(e));
-  return res.status(200).json({ users, statusCode: 200 });
+  return createResponse(res, { users }, 200);
 };
 
 exports.users_userId_GET = async (req, res, next) => {
   let user;
   if (req.cookieToken._id === req.params.userId) {
     user = await User.findById(req.params.userId)
-      .populate("friends requests posts")
+      .populate("friends", "name")
+      .populate("requests", "name")
+      .populate("posts")
       .catch((e) => next(e));
   } else {
     user = await User.findById(req.params.userId)
@@ -17,8 +20,6 @@ exports.users_userId_GET = async (req, res, next) => {
       .catch((e) => next(e));
   }
   if (!user)
-    return res
-      .status(404)
-      .json({ user: null, statusCode: 404, message: "User not found" });
-  return res.status(200).json({ user, statusCode: 200 });
+    return createResponse(res, { user: null, message: "User not found" }, 404);
+  return createResponse(res, { user }, 200);
 };
