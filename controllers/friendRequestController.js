289fdd151081598c4c6f32ad772ac8821292fa12
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-20 12:18:58 +0200

    Fix bug with pushing object instead of id and pull id on deletion

--- controllers/friendRequestController.js
+++ controllers/friendRequestController.js
@@ -13,50 +13,56 @@ exports.friendRequests_POST = [
     }),
   async (req, res, next) => {
     const errors = validationResult(body);
-    if (!errors.isEmpty)
+    if (!errors.isEmpty) {
       return res.status(400).json({
         statusCode: 400,
         message: "No receiving user was sent to the server",
         errors,
       });
+    }
 
     const sender = req.cookieToken._id;
     const receiver = req.body.receiver;
     const prevRequest = await FriendRequest.findOne({ sender, receiver }).catch(
       (e) => next(e)
     );
-    if (prevRequest)
+    if (prevRequest) {
       return res
         .status(400)
         .json({ message: "Request already sent.", statusCode: 400 });
+    }
 
-    const friendRequest = new FriendRequest({
-      sender,
-      receiver,
-    });
-    const friendRequestId = friendRequest._id;
-    const savedRequest = friendRequest.save().catch((e) => next(e));
+    try {
+      const friendRequest = new FriendRequest({
+        sender,
+        receiver,
+      });
+      const friendRequestId = friendRequest._id;
+      const savedRequest = friendRequest.save();
 
-    const savedSender = User.findByIdAndUpdate(sender, {
-      $push: { requests: friendRequest },
-    }).catch((e) => next(e));
+      const savedSender = User.findByIdAndUpdate(sender, {
+        $push: { requests: friendRequestId },
+      });
 
-    const savedReceiver = User.findByIdAndUpdate(receiver, {
-      $push: { requests: friendRequest },
-    }).catch((e) => next(e));
+      const savedReceiver = User.findByIdAndUpdate(receiver, {
+        $push: { requests: friendRequestId },
+      });
 
-    const waitForPushes = await Promise.all([
-      savedRequest,
-      savedSender,
-      savedReceiver,
-    ]).catch((e) => next(e));
+      const waitForPushes = await Promise.all([
+        savedRequest,
+        savedSender,
+        savedReceiver,
+      ]);
 
-    return res.status(200).json({
-      request: waitForPushes[0],
-      sender: waitForPushes[1].name,
-      receiver: waitForPushes[2].name,
-      message: "Friend request sent",
-    });
+      return res.status(200).json({
+        request: waitForPushes[0],
+        sender: waitForPushes[1].name,
+        receiver: waitForPushes[2].name,
+        message: "Friend request sent",
+      });
+    } catch (e) {
+      return next(e);
+    }
   },
 ];
 
@@ -65,8 +71,6 @@ exports.friendRequests_DELETE = async (req, res, next) => {
   const foundRequest = await FriendRequest.findById(requestId)
     .populate("sender receiver", "name")
     .catch((e) => next(e));
-  console.log(foundRequest.sender._id.toString());
-  console.log(req.cookieToken._id);
   if (!foundRequest)
     return res
       .status(404)
@@ -75,15 +79,43 @@ exports.friendRequests_DELETE = async (req, res, next) => {
     foundRequest.sender._id.toString() === req.cookieToken._id ||
     foundRequest.receiver._id.toString() === req.cookieToken._id
   ) {
-    const deletedRequest = await FriendRequest.findByIdAndDelete(
-      requestId
-    ).catch((e) => next(e));
-    return res
-      .status(200)
-      .json({ request: foundRequest, message: "Request has been deleted." });
+    try {
+      const deletedRequest = FriendRequest.findByIdAndDelete(requestId).exec();
+      const deletedSenderReq = User.findByIdAndUpdate(foundRequest.sender._id, {
+        $pull: { requests: requestId },
+      }).exec();
+
+      const deletedReceiverReq = User.findByIdAndUpdate(
+        foundRequest.receiver._id,
+        {
+          $pull: { requests: requestId },
+        }
+      ).exec();
+
+      const promises = await Promise.all([
+        deletedRequest,
+        deletedSenderReq,
+        deletedReceiverReq,
+      ]);
+      const foundRejection = promises?.find(
+        (promise) => promise.status === "rejected"
+      );
+      if (foundRejection) {
+        return next(
+          new Error("Something went wrong deleting this friend request")
+        );
+      }
+      return res
+        .status(200)
+        .json({ request: foundRequest, message: "Request has been deleted." });
+    } catch (e) {
+      return next(e);
+    }
   } else {
     return res
       .status(403)
       .json({ request: null, message: "You can't access this resource." });
   }
 };
+
+exports.friendRequests_ACCEPT = async (req, res, next) => {};
