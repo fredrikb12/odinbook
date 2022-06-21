58e86ccf58713daa0b47b1192d582dc4675d6590
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-21 11:49:24 +0200

    Add post field to like schema

--- models/like.js
+++ models/like.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 
 const LikeSchema = new Schema({
   user: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+  post: { type: Schema.Types.ObjectId, required: true, ref: "Post" },
 });
 
 module.exports = mongoose.model("Like", LikeSchema);
