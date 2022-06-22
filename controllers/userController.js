fabcee8a497668fcaf043b245c2a9ce5613f49f1
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-22 14:34:46 +0200

    Populate necessary fields with their respective document data

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,9 @@ const User = require("../models/user");
 const { createResponse } = require("../utils/responseHelpers");
 
 exports.users_GET = async (req, res, next) => {
-  const users = await User.find().catch((e) => next(e));
+  const users = await User.find()
+    .populate({ path: "requests", select: "sender receiver" })
+    .catch((e) => next(e));
   return createResponse(res, { users }, 200);
 };
 
@@ -11,12 +13,45 @@ exports.users_userId_GET = async (req, res, next) => {
   if (req.cookieToken._id === req.params.userId) {
     user = await User.findById(req.params.userId)
       .populate("friends", "name")
-      .populate("requests", "name")
-      .populate("posts")
+      .populate({
+        path: "requests",
+        populate: [
+          {
+            path: "sender",
+            select: "name",
+          },
+          { path: "receiver", select: "name" },
+        ],
+      })
+      .populate({
+        path: "posts",
+        populate: [
+          { path: "user", select: "name " },
+          { path: "likes", populate: { path: "user", select: "name" } },
+          { path: "comments", populate: { path: "user", select: "name" } },
+        ],
+      })
       .catch((e) => next(e));
   } else {
     user = await User.findById(req.params.userId)
-      .populate("friends posts")
+      .populate({
+        path: "friends",
+        select: "name",
+        populate: [
+          {
+            path: "name",
+            select: "",
+          },
+        ],
+      })
+      .populate({
+        path: "posts",
+        populate: [
+          { path: "user", select: "name " },
+          { path: "likes", populate: { path: "user", select: "name" } },
+          { path: "comments", populate: { path: "user", select: "name" } },
+        ],
+      })
       .catch((e) => next(e));
   }
   if (!user)
