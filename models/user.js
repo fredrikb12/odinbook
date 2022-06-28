cd7127f4a00e9443fe421311fa60302e57238c69
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-28 15:58:21 +0200

    Update model to allow different sign-in methods

--- models/user.js
+++ models/user.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+  username: { type: String, required: false, minlength: 3, maxlength: 30 },
+  password: { type: String, minlength: 5 },
   name: { type: String, required: true, maxLength: 100, minLength: 1 },
-  facebook_id: { type: String, required: true },
+  facebook_id: { type: String, required: false },
   picture: { type: String, required: false },
   friends: [
     { type: Schema.Types.ObjectId, ref: "User", required: true, default: [] },
