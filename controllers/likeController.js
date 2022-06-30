f6e15783c51423d5f2b00304587b9e7bff980b22
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-30 13:24:17 +0200

    Fix bug where users could like posts multiple times and implement like deletion

--- controllers/likeController.js
+++ controllers/likeController.js
@@ -36,6 +36,16 @@ exports.likes_POST = [
     const currentUserId = req.cookieToken._id;
     const postId = req.body.postId;
 
+    const foundPost = await Post.findById(postId)
+      .populate("likes")
+      .catch((e) => next(e));
+
+    if (
+      foundPost.likes.find((like) => like.user.toString() === currentUserId)
+    ) {
+      return createResponse(res, { message: "Post already liked." }, 400);
+    }
+
     try {
       const like = new Like({ post: postId, user: currentUserId });
       const savedLike = like.save();
@@ -44,6 +54,44 @@ exports.likes_POST = [
       });
       const promises = await Promise.all([savedLike, savedPost]);
       return createResponse(res, { message: "Post successfully liked." }, 200);
+    } catch (e) {
+      return next(e);
+    }
+  },
+];
+
+exports.likes_DELETE = [
+  body("postId", "Post ID must be supplied")
+    .exists()
+    .escape()
+    .custom(async (postId) => {
+      const foundPost = await Post.findById(postId).catch((e) =>
+        Promise.reject("Something went wrong finding this post")
+      );
+      if (!foundPost) {
+        return Promise.reject("No post with this id found");
+      } else {
+        return Promise.resolve(postId);
+      }
+    }),
+  async (req, res, next) => {
+    const postId = req.body.postId;
+    const currentUserId = req.cookieToken._id;
+
+    try {
+      const deletedLike = await Like.findOneAndDelete({
+        user: currentUserId,
+        post: postId,
+      });
+      const savedPost = Post.findByIdAndUpdate(postId, {
+        $pull: { likes: deletedLike._id },
+      });
+
+      return res.createResponse(
+        res,
+        { message: "Post successfully unliked" },
+        200
+      );
     } catch (e) {
       return next(e);
     }
