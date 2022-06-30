4b1ee44e82dace3e30fa7b20d75418cc2c280a65
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-30 13:54:35 +0200

    Add route for deleting likes

--- routes/likes.js
+++ routes/likes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { likes_POST } = require("../controllers/likeController");
+const { likes_POST, likes_DELETE } = require("../controllers/likeController");
 
 router.post("/", likes_POST);
+
+router.delete("/", likes_DELETE);
 
 module.exports = router;
